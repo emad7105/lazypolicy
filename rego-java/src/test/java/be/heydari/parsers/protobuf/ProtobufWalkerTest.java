8a44dd05d3a53c5a8d4311ce0cf521099a18b897
Emad Heydari Beni <emad@heydari.be>
2020-09-10 13:15:49 +0200

    Add some test and output some residual policies via Protobuff

--- rego-java/src/test/java/be/heydari/parsers/protobuf/ProtobufWalkerTest.java
+++ rego-java/src/test/java/be/heydari/parsers/protobuf/ProtobufWalkerTest.java
@@ -3,19 +3,35 @@ package be.heydari.parsers.protobuf;
 import be.heydari.ast.ResponseAST;
 import be.heydari.parsers.ResponseParser;
 import be.heydari.parsers.protobuf.generated.PDisjunction;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 
 import static be.heydari.ast.ParsingTest.stream;
+import static java.lang.String.format;
 import static org.junit.Assert.*;
 
 public class ProtobufWalkerTest {
     private static Logger logger = Logger.getLogger(ProtobufWalkerTest.class.getName());
+    private static String PROTOBUFS_PATH = "protobufs";
 
     private ResponseParser responseParser;
+
+    @BeforeClass
+    public static void init() throws IOException, URISyntaxException {
+        File f = new File(PROTOBUFS_PATH);
+        FileUtils.deleteDirectory(f);
+        FileUtils.forceMkdir(f);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -24,9 +40,12 @@ public class ProtobufWalkerTest {
 
     @Test
     public void processAstEq() throws IOException {
-        PDisjunction pDisjunction = parseAst("1-eq.json");
+        String file = "1-eq.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        logger.info(pDisjunction.toByteString().toStringUtf8());
+        writeToFile(file, pDisjunction);
 
         /*String expected = "\n" +
                 "(\u0012&\n" +
@@ -39,86 +58,110 @@ public class ProtobufWalkerTest {
 
     @Test
     public void processAstEqSpecificRow() throws IOException {
-        PDisjunction pDisjunction = parseAst("1-eq-specific-row.json");
+        String file = "1-eq-specific-row.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
     public void processAstGt() throws IOException {
-        PDisjunction pDisjunction = parseAst("2-gt.json");
+        String file = "2-gt.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
     public void processAstGtSpecificRow() throws IOException {
-        PDisjunction pDisjunction = parseAst("2-gt-specific-row.json");
+        String file = "2-gt-specific-row.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
     public void processAstGte() throws IOException {
-        PDisjunction pDisjunction = parseAst("3-gte.json");
+        String file = "3-gte.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
     public void processAstGteSpecificRow() throws IOException {
-        PDisjunction pDisjunction = parseAst("3-gte-specific-row.json");
+        String file = "3-gte-specific-row.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
     public void processAstLt() throws IOException {
-        PDisjunction pDisjunction = parseAst("4-lt.json");
+        String file = "4-lt.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstLtSpecificRow() throws IOException {
-        PDisjunction pDisjunction = parseAst("4-lt-specific-row.json");
+    public void processAstLtSpecificRow() throws IOException, URISyntaxException {
+        String file = "4-lt-specific-row.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstLte() throws IOException {
-        PDisjunction pDisjunction = parseAst("5-lte.json");
+    public void processAstLte() throws IOException, URISyntaxException {
+        String file = "5-lte.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstLteSpecificRow() throws IOException {
-        PDisjunction pDisjunction = parseAst("5-lte-specific-row.json");
+    public void processAstLteSpecificRow() throws IOException, URISyntaxException {
+        String file = "5-lte-specific-row.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstNeq() throws IOException {
-        PDisjunction pDisjunction = parseAst("6-neq.json");
+    public void processAstNeq() throws IOException, URISyntaxException {
+        String file = "6-neq.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstAnd() throws IOException {
-        PDisjunction pDisjunction = parseAst("8-and.json");
+    public void processAstAnd() throws IOException, URISyntaxException {
+        String file = "8-and.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
     @Test
-    public void processAstOr() throws IOException {
-        PDisjunction pDisjunction = parseAst("15-or.json");
+    public void processAstOr() throws IOException, URISyntaxException {
+        String file = "15-or.json";
+        PDisjunction pDisjunction = parseAst(file);
         assertNotNull(pDisjunction);
         logger.info(pDisjunction.toString());
+        writeToFile(file, pDisjunction);
     }
 
 
@@ -127,5 +170,10 @@ public class ProtobufWalkerTest {
 
         ProtobufWalker protobufWalker = new ProtobufWalker();
         return protobufWalker.processAst(responseAST, "AccountState");
+    }
+
+    private void writeToFile(String fileName, PDisjunction pDisjunction) throws IOException {
+        FileOutputStream fos = new FileOutputStream(format("%s/%s.protobytes",PROTOBUFS_PATH, fileName));
+        pDisjunction.writeTo(fos);
     }
 }
